e158226eb400a2dc96793210b6c71192f01fc92c
unknown <printfsergelen@gmail.com>
2024-02-14 22:26:25 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Header } from "./header/Header"
+
+
+export default function App(){
+  return(
+    <>
+    <div className="border-zinc-600	">
+      <Header />
+    </div>
+    </>
+  )
+}
---
+++ src/header/Header.tsx
@@ -0,0 +1,20 @@
+
+export const Header = () => {
+  return (
+    <>
+        <div className="bg-black h-14 flex justify-center">
+            <span className="text-slate-200 text-center my-auto">Complimentary U.S. No-Rush Shipping on orders of $95 or more. Shop now</span>
+        </div>
+        <div className="flex flex-row items-center pl-36 pr-8">
+            <img src="../src/assets/logo/Logo.svg" alt="Logo" />
+            <div>Shop</div>
+            <div>New Arrivals</div>
+            <div>Sales</div>
+            <div>Journal</div>
+
+            <div>
+            </div>
+        </div>
+    </>
+  )
+}
